0b7148cfb1854976a10f073db64867636f45d86a
AmitThakkar <vigildbest@gmail.com>
2016-07-13 15:39:25 +0530

    Adding to middleware function and code cleanup done.

--- src/index.js
+++ src/index.js
@@ -10,28 +10,50 @@ const GITHUB_LOGIN_URL = 'https://github.com/login';
 const OAUTH_ACCESS_TOKEN_PATH = '/oauth/access_token';
 const OAUTHORIZATION_PATH = '/oauth/authorize';
 
+let oauth2, redirectURL, clientId, clientSecret, redirectURI, scope;
+
 class NodeGithubOAuth {
     constructor(options) {
-        this.clientId = options.clientId;
-        this.clientSecret = options.clientSecret;
-        this.redirectURI = options.redirectURI;
-        this.scope = options.scope;
+        clientId = options.clientId;
+        clientSecret = options.clientSecret;
+        redirectURI = options.redirectURI;
+        scope = options.scope;
         this.initialize();
     }
 
     initialize() {
         let randomString = RANDOM_STRING.generate({});
-        this.oauth2 = SIMPLE_OAUTH2({
-            clientID: this.clientId,
-            clientSecret: this.clientSecret,
+        oauth2 = SIMPLE_OAUTH2({
+            clientID: clientId,
+            clientSecret: clientSecret,
             site: GITHUB_LOGIN_URL,
             tokenPath: OAUTH_ACCESS_TOKEN_PATH,
             authorizationPath: OAUTHORIZATION_PATH
         });
-        this.authorization_uri = oauth2.authCode.authorizeURL({
-            redirect_uri: this.redirectURI,
-            scope: this.scope,
+        redirectURL = oauth2.authCode.authorizeURL({
+            redirect_uri: redirectURI,
+            scope: scope,
             state: randomString
+        });
+    }
+
+    authorized() {
+        return (request, response) => {
+            response.redirect(redirectURL);
+        }
+    }
+
+    getToken(request, response, next) {
+        var code = request.query.code;
+        oauth2.authCode.getToken({
+            code: code,
+            redirect_uri: redirectURI
+        }, (error, result) => {
+            if (error) {
+                next(error);
+            } else {
+                next(null, result);
+            }
         });
     }
 }
